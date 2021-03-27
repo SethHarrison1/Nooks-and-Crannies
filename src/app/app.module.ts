34cd045e9bd6a8472319e462eba2fb138a75e4bb
vrdimarco <victoriadimarco99@gmail.com>
2021-03-27 15:06:27 -0400

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { App } from './app.page2.html/app.page2.html.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    App.Page2.HtmlComponent
   ],
   imports: [
     BrowserModule,
